76b669ae71425117291974cdb273d2fe3014c43e
Radu-A <elcastravete@gmail.com>
2023-05-22 20:17:06 +0200

    created/questions.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// INDEX.HTML
+
+// nos conectamos a la api
+async function printQuiz() {
+    try {
+        let res = await fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple");
+        let data = await res.json();
+        data.results.forEach(element=>{
+            console.log(element);
+
+        })
+    } catch {
+        console.log(`ERROR: ${error.stack}`);
+    }
+}
+printQuiz();
